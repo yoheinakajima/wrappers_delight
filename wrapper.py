9bd9c829f26f2836a90a5efc2cb10dde0b68ea32
Yohei Nakajima <me@yoheinakajima.com>
2023-08-12 15:55:53 -0700

    Update wrapper.py

--- wrapper.py
+++ wrapper.py
@@ -5,16 +5,6 @@ import csv
 # Save the original chat completion method
 _original_chat_completion = openai.ChatCompletion.create
 
-# A dictionary to store user-registered functions
-_registered_functions = {}
-
-def register_function(name, function):
-    """
-    Allow users to register their own functions which can be called 
-    by the assistant.
-    """
-    _registered_functions[name] = function
-
 def log_to_csv(params, response):
     with open("log.csv", mode='a', newline='', encoding='utf-8') as file:
         writer = csv.writer(file)
@@ -23,10 +13,9 @@ def log_to_csv(params, response):
 def _enhanced_chat_completion(*args, **kwargs):
     
     response = _original_chat_completion(*args, **kwargs)
-
-    # If there's a function_call in the response, log the structured data
-    if 'function_call' in response.choices[0]['message']:
-        log_to_csv(kwargs, response)
+    
+    # Log every response regardless of whether there's a function_call or not
+    log_to_csv(kwargs, response)
     
     return response
 
