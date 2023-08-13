4e2ebd78cdb785513e2df56fa39df4568cad4a0d
Yohei Nakajima <me@yoheinakajima.com>
2023-08-12 19:38:38 -0700

    Update wrapper.py

--- wrapper.py
+++ wrapper.py
@@ -1,22 +1,28 @@
 import openai
 import json
-import csv
 import datetime
 
 _original_chat_completion = openai.ChatCompletion.create
 
-def log_to_csv(params, response):
+def log_to_ndjson(params, response):
     timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     model = params.get("model", "")
     total_tokens = response["usage"]["total_tokens"]
     
-    with open("log.csv", mode='a', newline='', encoding='utf-8') as file:
-        writer = csv.writer(file)
-        writer.writerow([timestamp, json.dumps(params), json.dumps(response), total_tokens, model])
+    log_entry = {
+        "timestamp": timestamp,
+        "params": params,
+        "response": response,
+        "total_tokens": total_tokens,
+        "model": model
+    }
+    
+    with open("log.ndjson", mode='a', encoding='utf-8') as file:
+        file.write(json.dumps(log_entry) + '\n')
 
 def _enhanced_chat_completion(*args, **kwargs):
     response = _original_chat_completion(*args, **kwargs)
-    log_to_csv(kwargs, response)
+    log_to_ndjson(kwargs, response)
     return response
 
 openai.ChatCompletion.create = _enhanced_chat_completion
