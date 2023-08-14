9ed45b6e477c96c8d3444e6ad9c9e6ccf2613fa3
Yohei Nakajima <me@yoheinakajima.com>
2023-08-13 20:00:40 -0700

    Update wrapper.py

--- wrapper.py
+++ wrapper.py
@@ -4,25 +4,44 @@ import datetime
 
 _original_chat_completion = openai.ChatCompletion.create
 
-def log_to_ndjson(params, response):
-    timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    model = params.get("model", "")
-    total_tokens = response["usage"]["total_tokens"]
-    
-    log_entry = {
-        "timestamp": timestamp,
-        "params": params,
-        "response": response,
-        "total_tokens": total_tokens,
-        "model": model
-    }
-    
-    with open("log.ndjson", mode='a', encoding='utf-8') as file:
-        file.write(json.dumps(log_entry) + '\n')
+class ChatWrapper:
+    reflection_enabled = False
 
-def _enhanced_chat_completion(*args, **kwargs):
-    response = _original_chat_completion(*args, **kwargs)
-    log_to_ndjson(kwargs, response)
-    return response
+    @staticmethod
+    def enable_reflection():
+        ChatWrapper.reflection_enabled = True
 
-openai.ChatCompletion.create = _enhanced_chat_completion
+    @staticmethod
+    def disable_reflection():
+        ChatWrapper.reflection_enabled = False
+
+    @staticmethod
+    def log_to_ndjson(params, response):
+        timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        model = params.get("model", "")
+        total_tokens = response["usage"]["total_tokens"]
+
+        log_entry = {
+            "timestamp": timestamp,
+            "params": params,
+            "response": response,
+            "total_tokens": total_tokens,
+            "model": model
+        }
+
+        with open("log.ndjson", mode='a', encoding='utf-8') as file:
+            file.write(json.dumps(log_entry) + '\n')
+
+    @staticmethod
+    def enhanced_chat_completion(*args, **kwargs):
+        response = _original_chat_completion(*args, **kwargs)
+        ChatWrapper.log_to_ndjson(kwargs, response)
+
+        if ChatWrapper.reflection_enabled:
+            from .reflection import generate_prompt_reflection
+            generate_prompt_reflection(kwargs, response)
+
+        return response
+
+# Override the ChatCompletion with the enhanced version
+openai.ChatCompletion.create = ChatWrapper.enhanced_chat_completion
