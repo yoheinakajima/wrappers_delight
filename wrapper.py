1ff711ea7ead7c5dbaaaf362d66cb1affb5a505d
Yohei Nakajima <me@yoheinakajima.com>
2023-08-28 11:15:56 -0700

    Update wrapper.py
    Add ability to add taskLabel as an option to store (removed from OpenAI call).

--- wrapper.py
+++ wrapper.py
@@ -17,7 +17,7 @@ class ChatWrapper:
         ChatWrapper.reflection_enabled = False
 
     @staticmethod
-    def log_to_ndjson(params, response, response_time):
+    def log_to_ndjson(params, task_label, response, response_time):
         timestamp = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         model = params.get("model", "")
         total_tokens = response["usage"]["total_tokens"]
@@ -28,7 +28,8 @@ class ChatWrapper:
             "params": params,
             "response": response,
             "total_tokens": total_tokens,
-            "model": model
+            "model": model,
+            "taskLabel": task_label  # Add taskLabel here
         }
 
         with open("log.ndjson", mode='a', encoding='utf-8') as file:
@@ -37,11 +38,18 @@ class ChatWrapper:
     @staticmethod
     def enhanced_chat_completion(*args, **kwargs):
         timestamp_start = time.perf_counter()
+
+        # Extract and remove taskLabel from kwargs if it exists
+        task_label = kwargs.pop("taskLabel", None)
+
+        # Call the original OpenAI method
         response = _original_chat_completion(*args, **kwargs)
+
         timestamp_end = time.perf_counter()    
         response_time = timestamp_end - timestamp_start
         
-        ChatWrapper.log_to_ndjson(kwargs, response, response_time)
+        # Log with taskLabel
+        ChatWrapper.log_to_ndjson(kwargs, task_label, response, response_time)
 
         if ChatWrapper.reflection_enabled:
             from .reflection import generate_prompt_reflection
